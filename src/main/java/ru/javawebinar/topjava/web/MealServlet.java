c98bfe6239da497f46a7dba3f569467d19826758
Daniil <dlaskavy-ee25@lll.kpi.ua>
2022-08-16 16:36:24 +0200

    HW1-main_task

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,32 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.dao.MealDao;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class MealServlet extends HttpServlet {
+    private static final Logger log = getLogger(MealServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.debug("getting meals from MealsUtil");
+        request.setAttribute("meals",
+                MealsUtil.filteredByStreams(MealDao.getAll(), LocalTime.MIN, LocalTime.MAX, MealDao.getCaloriesPerDay()));
+
+        log.debug("forwarding to meals.jsp");
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+
+        //response.sendRedirect("meals.jsp");
+    }
+}
---
+++ src/main/webapp/meals.jsp
@@ -0,0 +1,36 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="java.time.format.DateTimeFormatter" %>
+
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h3><a href="index.html">Home</a></h3>
+<hr>
+<h2>Meals</h2>
+
+<table border="1">
+    <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Calories</th>
+        <th>Excess</th>
+    </tr>
+    <c:forEach items="${meals}" var="meal">
+
+        <tr <c:choose>
+            <c:when test="${meal.excess == true}"> style='color:darkred' </c:when>
+            <c:otherwise> style='color:green' </c:otherwise>
+        </c:choose>>
+            <td>${meal.dateTime.format( DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"))}</td>
+            <td>${meal.description}</td>
+            <td>${meal.calories}</td>
+            <td>${meal.excess}</td>
+        </tr>
+
+    </c:forEach>
+</table>
+</body>
+</html>
